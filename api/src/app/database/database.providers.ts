6fb852311b5a9a31c882043ea8b4ad0f5388636e
Hallison de Oliveira Brancalhão <ra-200556082@alunos.unicesumar.edu.br>
2023-09-25 21:47:08 -0300

    feat: criacao do módulo  conectado com imagem no docker

---
+++ api/src/app/database/database.providers.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
